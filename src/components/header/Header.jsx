66539ad4022fc485d8f6eac86e3102dfec81bea5
uklawson <uklawson@gmail.com>
2022-08-20 14:24:46 +0100

    upload image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import "./header.css"
+import "./header.css";
+import CTA from './CTA';
+import ME from '../../assets/pic.png'
 
 const Header = () => {
   return (
     <header>
-      <div className='.container header_container'>
+      <div className='container header__container'>
         <h5>Hello I am</h5>
         <h1>Shuhong Lu</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-        
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="myself" />
+        </div>
       </div>
     </header>
   )
