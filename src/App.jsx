05692bebe835856633eb03be3a7dc81d4d9cb10e
uklawson <uklawson@gmail.com>
2022-08-21 19:23:55 +0100

    delete testimonials services and testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
-import Contact from "./components/contact/Contact"
+import Contact from "./components/contact/Contact";
 import Experience from "./components/experience/Experience";
-import Footer from "./components/footer/Footer"
-import Services from "./components/services/Services";
-import Portfolio from "./components/portfolio/Portfolio"
-import Testimonials from "./components/testimonials/Testimonials"
+import Footer from "./components/footer/Footer";
+import Portfolio from "./components/portfolio/Portfolio";
 
 
 const App = () => {
@@ -17,9 +15,7 @@ const App = () => {
       <Nav />
       <About />
       <Experience />
-      <Services />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
 
